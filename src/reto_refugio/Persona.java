8a65329e2bca004189108989c7231989e26d397e
sortegaot <sortegaot@a.mikeldi.com>
2026-02-20 09:56:52 +0100

    Inicio de persona

---
+++ src/reto_refugio/Persona.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reto_refugio;
+
+/**
+ *
+ * @author s.ortega
+ */
+public class Persona {
+    private String nombre;
+    private String apellidos;
+
+    public Persona(String nombre, String apellidos) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellidos;
+    }
+    
+    
+}
