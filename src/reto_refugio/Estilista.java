8de2e6b8dede0e5be300662a50bd2ff8b19f44c1
sortegaot <sortegaot@a.mikeldi.com>
2026-02-23 12:59:22 +0100

    Inicio de estilista

---
+++ src/reto_refugio/Estilista.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reto_refugio;
+
+/**
+ *
+ * @author s.ortega
+ */
+public class Estilista extends Empleado{
+    
+    public Estilista(Object alta, float salario) {
+        super(alta, salario);
+    }
+    
+    public void tratar(Animal animal)
+    {
+        //falta que el animal puedad ser tratado
+    }
+}
