dce836c1bc1e2d38d9cea5ecc1d8c842652e6c34
Eukeni666 <efrescobe@a.mikeldi.com>
2026-02-23 13:33:36 +0100

    Clase Perro extends Mamifero

---
+++ src/reto_refugio/Perro.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reto_refugio;
+
+/**
+ *
+ * @author e.fresco
+ */
+public class Perro extends Mamifero {
+    private String tamanno; // P, M, G (pequeño, mediano, grande)
+}
