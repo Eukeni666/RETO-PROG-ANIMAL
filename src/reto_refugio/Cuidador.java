1b01d0897364bcebe5c3c7faa44021a0d684ff8d
sortegaot <sortegaot@a.mikeldi.com>
2026-02-23 12:39:21 +0100

    Inicio de cuidador

---
+++ src/reto_refugio/Cuidador.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reto_refugio;
+
+/**
+ *
+ * @author s.ortega
+ */
+public class Cuidador extends Empleado{
+    private Animal[] animales;
+
+    public Cuidador(Animal[] animales, Object alta, float salario) {
+        super(alta, salario);
+        this.animales = animales;
+    }
+
+    public Animal[] getAnimales() {
+        return animales;
+    }
+
+    public void setAnimales(Animal[] animales) {
+        this.animales = animales;
+    }
+    
+    public void alimentar(int cual)
+    {
+        if(cual < animales.length && cual >= 0)
+            animales[cual].comer();
+    }
+    
+    public void revisarSalud(int cual, String revision)
+    {
+        animales[cual].hacerRevision(revision);
+    }
+    
+    public void tratamiento(int cual)
+    {
+        animales[cual].setSalud(10);
+    }
+    
+    public void cuidar(int cual)
+    {
+        //¿Que es cuidar para nosostros?
+    }
+    
+    public void limpiar(int cual)
+    {
+        animales[cual].setHigiene(10);
+    }
+    
+    public Animal entregar(int cual)
+    {
+        return animales[cual];
+    }
+    
+    public void documentar()
+    {
+        //¿Como exactamente se desea?
+    }
+}
