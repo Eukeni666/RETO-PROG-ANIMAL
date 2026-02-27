8ae8ee506bc9ac470c5b602978a6482a24284bdb
sortegaot <sortegaot@a.mikeldi.com>
2026-02-27 08:48:06 +0100

    Avance en el gerente

--- src/reto_refugio/Gerente.java
+++ src/reto_refugio/Gerente.java
@@ -13,8 +13,8 @@ import java.time.LocalDate;
 public class Gerente extends Cuidador{
     private Animal[] animalesTot;
 
-    public Gerente(Animal[] animalesTot, Animal[] animales, LocalDate alta, float salario) {
-        super(animales, alta, salario);
+    public Gerente(Animal[] animalesTot, Animal[] animales, String nombre, String apellidos, LocalDate alta, float salario) {
+        super(animales, nombre, apellidos, alta, salario);
         this.animalesTot = animalesTot;
     }
 
@@ -39,6 +39,33 @@ public class Gerente extends Cuidador{
         System.arraycopy(cuidador.getAnimales(), 0, nuevos, 0, nuevos.length);
         nuevos[nuevos.length - 1] = animalesTot[animal];
         cuidador.setAnimales(nuevos);
+    }
+    
+    public Empleado contratar(Persona pers, LocalDate alta, float salario)
+    {
+        Empleado emp = new Empleado(pers.getNombre(), pers.getApellidos(), alta, salario);
+        emp.horario = "Horario no especificado";
+        return emp;
+    }
+    
+    public Empleado contratar(String nombre, String apellidos, LocalDate alta, float salario)
+    {
+        return new Empleado(nombre, apellidos, alta, salario);
+    }
+    
+    public void despedir(Empleado empleado)
+    {
+        empleado.vigente = false;
+    }
+    
+    public void modSalario(Empleado empleado, float salario)
+    {
+        empleado.setSalario(salario);
+    }
+    
+    public void setHorario(Empleado empleado, String horario)
+    {
+        empleado.horario = horario;
     }
     
     public void seguimiento()
